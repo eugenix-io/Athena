58ff5d7ee32519697d024455366eb38f8014adc4
Surya Vamsee <suryavamsee@Suryas-MacBook-Pro.local>
2024-04-03 18:23:57 +0530

    feat: update reward tracker interface with latest functions

--- src/interfaces/IRewardTracker.sol
+++ src/interfaces/IRewardTracker.sol
@@ -11,9 +11,11 @@ interface IRewardTracker {
     function unstake(address _depositToken, uint256 _amount) external;
     function unstakeForAccount(address _account, address _depositToken, uint256 _amount, address _receiver) external;
     function tokensPerInterval() external view returns (uint256);
-    function claim(address _receiver) external returns (uint256);
-    function claimForAccount(address _account, address _receiver) external returns (uint256);
-    function claimable(address _account) external view returns (uint256);
+    function claimRewards(address _receiver) external returns (uint256);
+    function claimRewardsForAccount(address _account, address _receiver) external returns (uint256);
+    function claimableRewards(address _account) external view returns (uint256);
+    function claimVestedTokens() external returns (uint256);
+    function claimVestedTokensForAccount(address _account, address _receiver) external returns (uint256);
     function averageStakedAmounts(address _account) external view returns (uint256);
     function cumulativeRewards(address _account) external view returns (uint256);
 }
