c20b13f1ecffa1cadd28293caaec31903fbc255d
KaitouKid1412 <vsurya.ayyagari@gmail.com>
2024-05-19 22:41:44 +0530

    feat: update last distributed time

--- src/LogxStaker.sol
+++ src/LogxStaker.sol
@@ -364,7 +364,8 @@ contract LogxStaker is IERC20, ILogxStaker, ReentrancyGuard, Governable {
             startTime: startTime
         });
         userIds[_account].push(stakeId);
-    }
+        lastRewardsTime[stakeId] = block.timestamp;
+    }   
 
     //Note - think of ways to make this function more gas efficient
     function _removeStake(address _account, bytes32 _stakeId) private {
